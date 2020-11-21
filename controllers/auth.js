c3c1e226f3c133c9549d5233bd192b363379abfa
Akshay Harith <akshay@webfluence.dev>
2020-11-21 20:55:36 +0530

    Add controller/route for login

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,9 +37,35 @@ exports.createAccount = (req, res) => {
 
 /* READ */
 exports.displayLogin = (req, res) => {
-	res.render('./auth/login', { title: 'Login to your account' });
+	res.render('./auth/login', { title: 'Login to your account', csrfToken: res.locals.csrfToken });
 };
 
 exports.displaySignup = (req, res) => {
-	res.render('./auth/register', { title: 'Get started' });
+	res.render('./auth/register', { title: 'Get started', csrfToken: res.locals.csrfToken });
+};
+
+exports.authenticateUser = (req, res) => {
+	const user = req.body.user;
+	const password = req.body.password;
+	let currentUser;
+	User.findOne({ $or: [ { username: user }, { email: user } ] })
+		.then((foundUser) => {
+			/* Redirect if user not found */
+			if (!foundUser) return res.redirect('/user/login');
+			currentUser = foundUser;
+			return bcrypt.compare(password, foundUser.password);
+		})
+		.then((passwordsMatch) => {
+			/* Redirect if authentication failed */
+			if (!passwordsMatch) return res.redirect('/user/login');
+			req.session.isLoggedIn = true;
+			req.session.user = currentUser;
+			req.session.save((error) => {
+				if (error) console.log(error);
+				res.redirect('/');
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 };
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/register', controller.displaySignup);
 router.post('/register', controller.createAccount);
+router.post('/login/auth', controller.authenticateUser);
 router.get('/login', controller.displayLogin);
 
 module.exports = router;
