4bb11d42c9e038e2b07749658dc268249492d749
Akshay Harith <akshay@webfluence.dev>
2020-11-21 20:03:45 +0530

    Add controller for user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,41 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
+/* CREATE */
+exports.createAccount = (req, res) => {
+	const username = req.body.username;
+	const email = req.body.email;
+	const password = req.body.password;
+	const c_password = req.body.c_password;
+	User.findOne({ $or: [ { username: username }, { email: email } ] })
+		.then((foundUser) => {
+			/* Reject account creation if details already exist */
+			if (foundUser) return res.redirect('/user/register');
+			/* Reject account creation if password fields do not match */
+			if (password !== c_password) return res.redirect('/user/register');
+			return bcrypt.hash(c_password, 12);
+		})
+		.then((encryptedPassword) => {
+			const user = new User({
+				username: username,
+				email: email,
+				password: encryptedPassword,
+			});
+			console.log(
+				`A new user has been created with the following details:\nUsername: ${user.username}\nEmail:${user.email}`
+			);
+			return user.save();
+		})
+		.then((newUser) => {
+			res.redirect('/');
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+/* READ */
 exports.displayLogin = (req, res) => {
 	res.render('./auth/login', { title: 'Login to your account' });
 };
