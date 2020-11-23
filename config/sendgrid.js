2daa6e0b8c32c337fe3bdc5e364811d050781709
Akshay Harith <akshay@webfluence.dev>
2020-11-23 21:51:30 +0530

    FIX: Erros in API key

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
-const apiKey = process.env.SENDGRID_API;
 
 const transporter = nodemailer.createTransport(
 	sendgridTransport({
 		auth: {
-			api_key: process.env.SENDGRID_API,
+			api_key: `${process.env.SENDGRID_API_KEY}`,
 		},
 	})
 );
+
+module.exports = transporter;
