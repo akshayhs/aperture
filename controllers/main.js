8df4b579f742bc888fa52723fd801112341c2136
Akshay Harith <akshay@webfluence.dev>
2020-11-25 18:01:27 +0530

    ADD: logic for contact

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,68 @@
 const User = require('../models/user');
+const Enquiries = require('../models/contact');
+const transporter = require('../config/sendgrid');
+const { Schema } = require('mongoose');
+
+/* CREATE */
+exports.submitResponse = (req, res) => {
+	const firstname = req.body.firstname;
+	const lastname = req.body.lastname;
+	const email = req.body.email;
+	const phone = req.body.phone;
+	const reason = req.body.reason;
+	const message = req.body.message;
+	const enquiry = new Enquiries({
+		firstName: firstname,
+		lastName: lastname,
+		email,
+		phone,
+		reason,
+		message,
+	});
+	let details;
+	enquiry
+		.save()
+		.then((savedInfo) => {
+			details = savedInfo;
+			res.redirect('/contact');
+			const mailToUser = {
+				from: 'ak.prodigy24@gmail.com',
+				to: savedInfo.email,
+				subject: 'Your enquiry on Aperture',
+				html: `Hey, ${savedInfo.firstName}!\n
+				Thank you for writing to us on Aperture.
+			`,
+			};
+			return transporter.sendMail(mailToUser, (error, result) => {
+				if (error) console.log(error);
+				console.log(result);
+			});
+		})
+		.then((notifyAdmin) => {
+			const email = {
+				to: 'akshay@webfluence.dev',
+				from: 'ak.prodigy24@gmail.com',
+				subject: `New contact request from ${details.firstName} ${details.lastName}`,
+				html: `You have received a new contact request with the following details:\n
+				First Name: ${details.firstName}\n
+				Last Name: ${details.lastName}\n
+				Email: ${details.email}\n
+				Phone: ${details.phone}\n
+				Reason: ${details.reason}\n
+				Message: ${details.message}\n
+				`,
+			};
+			transporter.sendMail(email, (error, success) => {
+				if (error) console.log(error);
+				console.log(success);
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
 
 exports.displayIndex = (req, res) => {
-	console.log(res.locals.currentUser);
 	res.render('./index', {
 		title: 'Welcome!',
 		user: res.locals.loggedInUser,
@@ -49,6 +110,7 @@ exports.displayContact = async (req, res) => {
 		title: 'Blogs/discussions',
 		user: res.locals.loggedInUser,
 		isAuthenticated: res.locals.isAuthenticated,
+		csrfToken: res.locals.csrfToken,
 	});
 };
 
