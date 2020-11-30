caeb9277b27ae1460f95489e6bb50106cc91f66e
Akshay Harith <akshay@webfluence.dev>
2020-12-01 02:09:18 +0530

    ADD: Image model

---
+++ models/image.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const moment = require('moment-timezone');
+const dateIndia = moment.tz(Date.now(), 'Asia/Calcutta');
+
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema({
+	createdBy: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	path: {
+		type: String,
+		required: true,
+	},
+	category: {
+		type: String,
+		required: true,
+		enum: [ 'abstract', 'creative', 'landscape', 'monochrome', 'nature', 'open', 'pictorial', 'street', 'travel' ],
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	camera: {
+		type: String,
+		required: true,
+	},
+	lens: {
+		type: String,
+		required: true,
+	},
+	exposure: {
+		type: String,
+		required: true,
+	},
+	shutterspeed: {
+		type: String,
+		required: true,
+	},
+	pptechniques: {
+		type: String,
+	},
+	copyright: {
+		type: Boolean,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: dateIndia,
+	},
+	updatedAt: {
+		type: Date,
+	},
+});
+
+module.exports = mongoose.model('Image', imageSchema);
