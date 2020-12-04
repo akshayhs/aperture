b0b622333d36fb95c88c89ffac731c6c31cd98dd
Akshay Harith <akshay@webfluence.dev>
2020-12-04 20:02:25 +0530

    ADD: route to create blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,8 +1,72 @@
+const Blog = require('../models/blog');
+const User = require('../models/user');
+
+/* CREATE */
+
+exports.saveBlog = (req, res) => {
+	const author = req.session.user;
+	const title = req.body.title;
+	const blog = new Blog({ title, author });
+	let post;
+	blog
+		.save()
+		.then((savedBlog) => {
+			post = savedBlog;
+			return User.findOne({ username: author });
+		})
+		.then((foundUser) => {
+			if (!foundUser) {
+				return res.redirect('/');
+			}
+			return foundUser._id.toString();
+		})
+		.then((savedPost) => {
+			res.redirect('/blogs');
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+/* READ */
+
 exports.createBlog = (req, res) => {
-	res.render('./blog/add', {
-		title: 'Ad your blog',
+	res.status(200).render('./blog/add', {
+		title: 'Add your blog',
+		isAuthenticated: res.locals.isAuthenticated,
 		user: res.locals.loggedInUser,
 		csrfToken: res.locals.csrfToken,
-		isAuthenticated: res.locals.isAuthenticated,
 	});
+};
+
+exports.displayBlog = (req, res) => {
+	const id = req.params.id;
+	Blog.findById({ _id: id }).then((blog) => {
+		if (!blog) {
+			return;
+		}
+		res.status(200).render('./blog/display', {
+			title: `${blog.title}`,
+			isAuthenticated: res.locals.isAuthenticated,
+			user: res.locals.loggedInUser,
+			blog: blog,
+		});
+	});
+};
+
+/* UPDATE */
+
+/* DELETE */
+exports.deleteBlog = (req, res) => {
+	const id = req.params.id;
+	Blog.findByIdAndDelete({ _id: id })
+		.then((blog) => {
+			if (!blog) {
+				return;
+			}
+			res.status(200).redirect('/');
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 };
--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const controller = require('../controllers/blog');
-const mainController = require('../controllers/main');
 
 const router = express.Router();
 
+router.post('/success', controller.saveBlog);
 router.get('/add', controller.createBlog);
-router.get('/', mainController.displayBlogs);
+router.get('/:id/delete', controller.deleteBlog);
+router.get('/:id', controller.displayBlog);
 
 module.exports = router;
