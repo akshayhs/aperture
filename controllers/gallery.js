19011ef4ad5cc2e9d01b42bd14d9bc33aff8a89e
Akshay Harith <akshay@webfluence.dev>
2020-12-05 19:17:22 +0530

    REMOVE: Route and controller for gallery

--- controllers/gallery.js
+++
@@ -1,9 +0,0 @@
-exports.displayUpload = (req, res) => {
-	res.status(200).render('./image/add', {
-		title: 'Add your image',
-		isAuthenticated: res.locals.isAuthenticated,
-		csrfToken: res.locals.csrfToken,
-		user: res.locals.loggedInUser,
-	});
-};
-
--- routes/gallery.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/gallery');
-const router = express.Router();
-
-router.get('/add', controller.displayUpload);
-
-
-module.exports = router;
