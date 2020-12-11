18421dbe5ac370ac38219ff10b652125c6cf61b1
Akshay Harith <akshay@webfluence.dev>
2020-12-11 21:15:10 +0530

    MODIFY: Remove date-related fields

--- models/image.js
+++ models/image.js
@@ -4,69 +4,75 @@ const dateIndia = moment.tz(Date.now(), 'Asia/Calcutta');
 
 const Schema = mongoose.Schema;
 
-const imageSchema = new Schema({
-	createdBy: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
+const imageSchema = new Schema(
+	{
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		path: {
+			type: String,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+			enum: [
+				'abstract',
+				'creative',
+				'landscape',
+				'monochrome',
+				'nature',
+				'open',
+				'pictorial',
+				'street',
+				'travel',
+			],
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		tags: {
+			type: Array,
+		},
+		camera: {
+			type: String,
+			required: true,
+		},
+		lens: {
+			type: String,
+			required: true,
+		},
+		exposure: {
+			type: String,
+			required: true,
+		},
+		shutterspeed: {
+			type: String,
+			required: true,
+		},
+		sensitivity: {
+			type: Number,
+			required: true,
+		},
+		pptechniques: {
+			type: String,
+			maxlength: 140,
+			default: 'No information has been provided by the author.',
+		},
+		copyright: {
+			type: Boolean,
+			required: true,
+		},
+		critiques: [ { type: Schema.Types.ObjectId, ref: 'Comment' } ],
 	},
-	path: {
-		type: String,
-		required: true,
-	},
-	category: {
-		type: String,
-		required: true,
-		enum: [ 'abstract', 'creative', 'landscape', 'monochrome', 'nature', 'open', 'pictorial', 'street', 'travel' ],
-	},
-	title: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	tags: {
-		type: Array,
-	},
-	camera: {
-		type: String,
-		required: true,
-	},
-	lens: {
-		type: String,
-		required: true,
-	},
-	exposure: {
-		type: String,
-		required: true,
-	},
-	shutterspeed: {
-		type: String,
-		required: true,
-	},
-	sensitivity: {
-		type: Number,
-		required: true,
-	},
-	pptechniques: {
-		type: String,
-		maxlength: 140,
-		default: 'No information has been provided by the author.',
-	},
-	copyright: {
-		type: Boolean,
-		required: true,
-	},
-	createdAt: {
-		type: Date,
-		default: dateIndia,
-	},
-	updatedAt: {
-		type: Date,
-	},
-	critiques: [ { type: Schema.Types.ObjectId, ref: 'Comment' } ],
-});
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('Image', imageSchema);
