f3cf3cbbfd11b354079613d018cf545854261a6c
Akshay Harith <akshay@webfluence.dev>
2020-12-15 01:49:24 +0530

    FIX: Comment date

--- models/blog.js
+++ models/blog.js
@@ -4,39 +4,35 @@ const Schema = mongoose.Schema;
 
 const dateIndia = moment.tz(Date.now(), 'Asia/Calcutta');
 
-const blogSchema = new Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	abstract: {
-		type: String,
-		required: true,
-		maxlength: 60,
-	},
-	description: {
-		type: String,
-		required: true,
-		maxlength: 3000,
-	},
-	createdAt: {
-		type: Date,
-		default: dateIndia,
-	},
-	updatedAt: {
-		type: Date,
-	},
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	comments: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Comment',
+const blogSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
 		},
-	],
-});
+		abstract: {
+			type: String,
+			required: true,
+			maxlength: 60,
+		},
+		description: {
+			type: String,
+			required: true,
+			maxlength: 3000,
+		},
+		author: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		comments: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Comment',
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('Blog', blogSchema);
