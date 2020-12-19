95ecfb423fd08d24e12b1815e535f039f747d541
Akshay Harith <akshay@webfluence.dev>
2020-12-20 00:24:08 +0530

    ADD: routes for editing images and deleting user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/users');
 const auth = require('../middleware/auth');
-
 const router = express.Router();
 
+router.delete('/:username', auth, controller.deleteAccount);
+router.patch('/:username/profile', auth, controller.saveUserDetails);
 router.post('/:username/profile', auth, controller.completeUserProfile);
 router.get('/:username/profile', auth, controller.displayProfile);
 router.get('/:username/profile/complete', auth, controller.completeProfile);
