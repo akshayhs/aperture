53854d9622d40fa8e7846a557427c900f1b25dc7
Akshay Harith <akshay@webfluence.dev>
2021-02-03 01:50:06 +0530

    FIX: HTTP Route verb for user log out

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,20 @@ const controller = require('../controllers/auth');
 
 const router = express.Router();
 
-router.patch('/user/password/reset/:token/confirm', controller.confirmUserPasswordReset);
-router.get('/user/password/reset/:token', controller.attemptUserPasswordReset);
-router.post('/user/password/reset/', controller.resetUserPassword);
+/* ========================
+  PASSWORD RELATED
+========================== */
+router.patch('/user/password/reset/:token/confirm', controller.confirmUserPasswordReset); // Actual password recovery route
+router.get('/user/password/reset/:token', controller.attemptUserPasswordReset); // Will be sent part of the email
+router.post('/user/password/reset/', controller.resetUserPassword); // After user enters email
 router.get('/user/password/reset', controller.displayPasswordResetForm);
+/* =========================
+  AUTH ROUTES
+==========================*/
 router.get('/register', controller.displaySignup);
 router.post('/register', controller.createAccount);
 router.post('/login/succeeded', controller.authenticateUser);
 router.get('/login', controller.displayLogin);
-router.get('/logout', controller.attemptLogout);
+router.post('/logout', controller.attemptLogout);
 
 module.exports = router;
